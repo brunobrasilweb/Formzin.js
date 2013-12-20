82be1105926d90f0d28ad45ce256b223654fccab
Bruno Brasil <brunobrasilweb@gmail.com>
2013-12-20 21:33:50 -0200

    atualizacao versao 0.2

--- js/formzin.js
+++ js/formzin.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Formzin: formzin.js v0.1
+ * Formzin: formzin.js v0.2
  * http://brunobrasilweb.github.io/Formzin.js
  * ========================================================================
  * Copyright (c) 2013 @brunobrasilweb.
@@ -19,135 +19,59 @@
 var Formzin = {
     iniciar: function() {
         jQuery(document).ready(function() {
-            Formzin.formatacao();
-            Formzin.validacaoReal();
-            Formzin.manipulacao();
+            Formzin.formatar(jQuery('.cpf'), "000.000.000-00");
+            Formzin.formatar(jQuery('.cnpj'), "00.000.000/0000-00");
+            Formzin.formatar(jQuery('.cep'), "00000-000");
+            Formzin.formatar(jQuery('.telefone'), "(00) 0000-00000");
+            Formzin.formatar(jQuery('.cartao_credito'), "0000 0000 0000 0000");
+            Formzin.formatar(jQuery('.inteiro'), "inteiro");
+            Formzin.formatar(jQuery('.mascara'), "mascara");
+            Formzin.formatar(jQuery('.caixa_alta'), "caixa_alta");
+            Formzin.formatar(jQuery('.data'), "data");
+            Formzin.formatar(jQuery('.placa_carro'), "SSS 0000");
+            Formzin.formatar(jQuery('.codigo_rastreio'), "SS000000000SS");
+            Formzin.fMoeda();
+
+            Formzin.vCampo();
+            Formzin.validar(jQuery('.validar-email'), Formzin.validarEmail, 'Por favor preenchar um e-mail v&aacute;lido.');
+            Formzin.validar(jQuery('.validar-url'), Formzin.validarUrl, 'Por favor preenchar uma url v&aacute;lida.');
+            Formzin.validar(jQuery('.validar-cpf'), Formzin.validarCpf, 'Por favor preenchar o CPF corretamente.');
+            Formzin.validar(jQuery('.validar-cnpj'), Formzin.validarCnpj, 'Por favor preenchar o CNPJ corretamente.');
+            Formzin.validar(jQuery('.validar-data'), Formzin.validarData, 'Por favor preenchar a data corretamente.');
+
+            Formzin.mBuscarEndereco();
         });
     },
-    validacaoReal: function() {
-        //Validar Campo
-        Formzin.vCampo();
-        //Validar CPF
-        Formzin.vCpf();
-        //Validar CNPJ
-        Formzin.vCnpj();
-        //Validar Data
-        Formzin.vData();
-    },
-    formatacao: function() {
-        // CPF
-        Formzin.fCpf();
-        // CNPJ
-        Formzin.fCnpj();
-        // Inteiro
-        Formzin.fInteiro();
-        // Data
-        Formzin.fData();
-        // CEP
-        Formzin.fCep();
-        // Moeda
-        Formzin.fMoeda();
-        // Caixa Alta
-        Formzin.fCaixaAlta();
-        // Telefone
-        Formzin.fTelefone();
-        // Cartão de Crédito
-        Formzin.fCartaoCredito();
-        // Mascara
-        Formzin.fMascara();
-    },
-    manipulacao: function() {
-        // Buscar endereço
-        Formzin.mBuscarEndereco();
-    },
-    fMascara: function() {
-        var mascara = jQuery(".mascara");
-        mascara.bind('keyup', function() {
-            var v = jQuery(this).val();
-            v = Formzin.formatarCampo(jQuery(this), jQuery(this).attr("formato"));
-            jQuery(this).val(v);
-        });
-    },
-    fCpf: function() {
-        var cpf = jQuery(".cpf");
-        cpf.attr("maxlength", "14");
-        cpf.bind('keyup', function() {
-            var v = jQuery(this).val();
-            v = Formzin.formatarCampo(jQuery(this), "000.000.000-00");
-            jQuery(this).val(v);
-        });
-    },
-    fCnpj: function() {
-        var cnpj = jQuery(".cnpj");
-        cnpj.attr("maxlength", "18");
-        cnpj.bind('keyup', function() {
-            var v = jQuery(this).val();
-            v = Formzin.formatarCampo(jQuery(this), "00.000.000/0000-00");
-            jQuery(this).val(v);
-        });
-    },
-    fInteiro: function() {
-        jQuery(".inteiro").bind('keydown keyup', function() {
-            var v = jQuery(this).val().replace(/\D/g, "");
-            jQuery(this).val(v);
-        });
-    },
-    fData: function() {
-        var data = jQuery(".data");
-        data.attr("maxlength", "10");
-        data.bind('keyup', function() {
-            var separador = jQuery(this).attr('separador');
+    formatar: function(obj, formato) {
+        obj.val(Formzin.formatarCampo(obj, formato));
+        
+        if (formato == "caixa_alta")
+            jQuery('.caixa_alta').attr("style", "text-transform: uppercase;");
+
+        obj.bind('keyup', function() {
             var v = jQuery(this).val();
 
-            if (!separador)
-                separador = "/";
+            if (formato == "inteiro") {
+                v = v.replace(/\D/g, "");
+            } else if (formato == "mascara") {
+                v = Formzin.formatarCampo(jQuery(this), jQuery(this).attr("formato"));
+            } else if (formato == "caixa_alta") {
+                v = v.toUpperCase();
+            } else if (formato == "data") {
+                var separador = jQuery(this).attr('separador');
 
-            v = Formzin.formatarCampo(jQuery(this), "00" + separador + "00" + separador + "0000");
+                if (!separador)
+                    separador = "/";
+
+                v = Formzin.formatarCampo(jQuery(this), "00" + separador + "00" + separador + "0000");
+            } else {
+                v = Formzin.formatarCampo(jQuery(this), formato);
+            }
 
             jQuery(this).val(v);
         });
     },
-    fCep: function() {
-        var cep = jQuery(".cep");
-        cep.attr("maxlength", "9");
-        cep.bind('keyup', function() {
-            var v = jQuery(this).val();
-            v = Formzin.formatarCampo(jQuery(this), "00000-000");
-
-            jQuery(this).val(v);
-        });
-    },
-    fCaixaAlta: function() {
-        var caixaAlta = jQuery(".caixa_alta");
-        caixaAlta.attr("style", "text-transform: uppercase;");
-        caixaAlta.bind('keydown keyup', function() {
-            var v = jQuery(this).val();
-            v = v.toUpperCase();
-
-            jQuery(this).val(v);
-        });
-    },
-    fTelefone: function() {
-        var tel = jQuery(".telefone");
-        tel.attr("maxlength", "15");
-        tel.bind('keyup', function() {
-            var v = jQuery(this).val();
-            v = Formzin.formatarCampo(jQuery(this), "(00) 0000-00000");
-
-            jQuery(this).val(v);
-        });
-    },
-    fCartaoCredito: function() {
-        var cartao = jQuery(".cartao_credito");
-        cartao.attr("maxlength", "19");
-        cartao.bind('keyup', function() {
-            var v = jQuery(this).val();
-            v = Formzin.formatarCampo(jQuery(this), "0000 0000 0000 0000");
-
-            jQuery(this).val(v);
-        });
-    },
-    fMoeda: function() { // crédito ao site jquerypriceformat.com
+    fMoeda: function() {
         var moeda = jQuery(".moeda");
         moeda.bind('keydown keyup', function() {
             var obj = jQuery(this);
@@ -222,79 +146,21 @@ var Formzin = {
                 if (jQuery(this).attr('msn_validacao'))
                     jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">' + jQuery(this).attr('msn_validacao') + '</span>');
                 else
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor preenchar o CPF corretamente.</span>');
+                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor preenchar campo.</span>');
             } else if (jQuery(this).attr('minlength')) {
                 if (jQuery(this).val().length < jQuery(this).attr('minlength'))
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor o campo deve ter no mínimo (' + jQuery(this).attr('minlength') + ') caracteres.</span>');
+                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor o campo deve ter no m&iacute;nimo (' + jQuery(this).attr('minlength') + ') caracteres.</span>');
             } else if (jQuery(this).attr('maxlength')) {
                 if (jQuery(this).val().length > jQuery(this).attr('maxlength'))
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor o campo deve ter no máximo (' + jQuery(this).attr('maxlength') + ') caracteres.</span>');
+                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor o campo deve ter no m&aacute;ximo (' + jQuery(this).attr('maxlength') + ') caracteres.</span>');
             }
         });
     },
-    vCpf: function() {
-        var cpf = jQuery(".validar-cpf");
-        cpf.attr("maxlength", "14");
-        cpf.bind('keydown keyup change', function() {
+    validar: function(obj, func, msn) {
+        obj.bind('keydown keyup change', function() {
             var v = jQuery(this).val();
-            var id = jQuery(this).attr("id");
-            valido = Formzin.validarCpf(v);
-            if (valido == false) {
-                jQuery(".error-validacao-" + id).remove();
-                jQuery(this).attr('style', 'border: 1px solid red;');
-                jQuery(this).addClass('campo-validacao-' + id);
-                if (jQuery(this).attr('msn_validacao'))
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">' + jQuery(this).attr('msn_validacao') + '</span>');
-                else
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor preenchar o CPF corretamente.</span>');
-            } else {
-                jQuery(this).removeAttr('style', 'border: 1px solid red;');
-                jQuery(this).removeClass('campo-validacao-' + id);
-                jQuery(".error-validacao-" + id).remove();
-            }
-        });
-    },
-    vCnpj: function() {
-        var cnpj = jQuery(".validar-cnpj");
-        cnpj.bind('keydown keyup change', function() {
-            var v = jQuery(this).val();
-            var id = jQuery(this).attr("id");
-            valido = Formzin.validarCnpj(v);
-            if (valido == false) {
-                jQuery(".error-validacao-" + id).remove();
-                jQuery(this).attr('style', 'border: 1px solid red;');
-                jQuery(this).addClass('campo-validacao-' + id);
-                if (jQuery(this).attr('msn_validacao'))
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">' + jQuery(this).attr('msn_validacao') + '</span>');
-                else
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor preenchar o CNPJ corretamente.</span>');
-            } else {
-                jQuery(this).removeAttr('style', 'border: 1px solid red;');
-                jQuery(this).removeClass('campo-validacao-' + id);
-                jQuery(".error-validacao-" + id).remove();
-            }
-        });
-    },
-    vData: function() {
-        var data = jQuery(".validar-data");
-        data.bind('keydown keyup change', function() {
-            var v = jQuery(this).val();
-            var id = jQuery(this).attr("id");
-
-            valido = Formzin.validarData(v);
-            if (valido == false) {
-                jQuery(".error-validacao-" + id).remove();
-                jQuery(this).attr('style', 'border: 1px solid red;');
-                jQuery(this).addClass('campo-validacao-' + id);
-                if (jQuery(this).attr('msn_validacao'))
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">' + jQuery(this).attr('msn_validacao') + '</span>');
-                else
-                    jQuery(this).after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">Por favor preenchar a data corretamente.</span>');
-            } else {
-                jQuery(this).removeAttr('style', 'border: 1px solid red;');
-                jQuery(this).removeClass('campo-validacao-' + id);
-                jQuery(".error-validacao-" + id).remove();
-            }
+            valido = func(v);
+            Formzin.msnValidacao(jQuery(this), valido, msn);
         });
     },
     mBuscarEndereco: function() {
@@ -311,56 +177,92 @@ var Formzin = {
                     });
         });
     },
-    formatarCampo: function(obj, mask, evt) {
-        try {
-            var value = obj.val();
-
-            // If user pressed DEL or BACK SPACE, clean the value
-            try {
-                var e = (evt.which) ? evt.which : event.keyCode;
-                if (e == 46 || e == 8) {
-                    obj.val("");
-                    return;
-                }
-            } catch (e1) {
-            }
-
-            var literalPattern = /[0\*]/;
-            var numberPattern = /[0-9]/;
-            var newValue = "";
-
-            for (var vId = 0, mId = 0; mId < mask.length; ) {
-                if (mId >= value.length)
-                    break;
-
-                // Number expected but got a different value, store only the valid portion
-                if (mask[mId] == '0' && value[vId].match(numberPattern) == null) {
-                    break;
-                }
-
-                // Found a literal
-                while (mask[mId].match(literalPattern) == null) {
-                    if (value[vId] == mask[mId])
-                        break;
-
-                    newValue += mask[mId++];
-                }
-
-                newValue += value[vId++];
-                mId++;
-            }
-
-            return newValue;
-        } catch (e) {
+    msnValidacao: function(obj, valido, msn) {
+        var id = obj.attr('id');
+        if (valido == false) {
+            jQuery(".error-validacao-" + id).remove();
+            obj.attr('style', 'border: 1px solid red;');
+            obj.addClass('campo-validacao-' + id);
+            if (obj.attr('msn_validacao'))
+                obj.after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">' + obj.attr('msn_validacao') + '</span>');
+            else
+                obj.after('<span class="error-validacao error-validacao-' + id + '" style="color:red;">' + msn + '</span>');
+        } else {
+            obj.removeAttr('style', 'border: 1px solid red;');
+            obj.removeClass('campo-validacao-' + id);
+            jQuery(".error-validacao-" + id).remove();
         }
     },
-    validarCpf: function(cpf) { // Credito ao site www.geradorcpf.com
+    formatarCampo: function(obj, mask, skipMaskChars) {
+        var trans = {
+            '0': {pattern: /\d/},
+            'A': {pattern: /[a-zA-Z0-9]/},
+            'S': {pattern: /[a-zA-Z]/}
+        };
+
+        var buf = [],
+                value = obj.val(),
+                m = 0, maskLen = mask.length,
+                v = 0, valLen = value.length,
+                offset = 1, addMethod = "push",
+                lastMaskChar,
+                check;
+
+        lastMaskChar = maskLen - 1;
+        check = function() {
+            return m < maskLen && v < valLen;
+        };
+
+        while (check()) {
+
+            var maskDigit = mask.charAt(m),
+                    valDigit = value.charAt(v),
+                    translation = trans[maskDigit];
+
+            if (translation) {
+                if (valDigit.match(translation.pattern)) {
+                    buf[addMethod](valDigit);
+                    m += offset;
+                } else if (translation.optional) {
+                    m += offset;
+                    v -= offset;
+                }
+                v += offset;
+            } else {
+                if (skipMaskChars === undefined) {
+                    buf[addMethod](maskDigit);
+                }
+
+                if (valDigit === maskDigit) {
+                    v += offset;
+                }
+
+                m += offset;
+            }
+        }
+        
+        return buf.join("");
+    },
+    validarEmail: function(email) {
+        var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!pattern.test(email))
+            return false;
+
+        return true;
+    },
+    validarUrl: function(url) {
+        var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        if (pattern.test(url))
+            return true;
+        else
+            return false;
+    },
+    validarCpf: function(cpf) {
         cpf = cpf.replace(/[^\d]+/g, '');
 
         if (cpf == '')
             return false;
 
-        // Elimina CPFs invalidos conhecidos
         if (cpf.length != 11 ||
                 cpf == "00000000000" ||
                 cpf == "11111111111" ||
@@ -374,7 +276,6 @@ var Formzin = {
                 cpf == "99999999999")
             return false;
 
-        // Valida 1o digito
         add = 0;
         for (i = 0; i < 9; i ++)
             add += parseInt(cpf.charAt(i)) * (10 - i);
@@ -384,7 +285,6 @@ var Formzin = {
         if (rev != parseInt(cpf.charAt(9)))
             return false;
 
-        // Valida 2o digito
         add = 0;
         for (i = 0; i < 10; i ++)
             add += parseInt(cpf.charAt(i)) * (11 - i);
@@ -396,7 +296,7 @@ var Formzin = {
 
         return true;
     },
-    validarCnpj: function(cnpj) { // crédito ao site www.geradorcnpj.com
+    validarCnpj: function(cnpj) {
 
         cnpj = cnpj.replace(/[^\d]+/g, '');
 
@@ -406,7 +306,6 @@ var Formzin = {
         if (cnpj.length != 14)
             return false;
 
-        // Elimina CNPJs invalidos conhecidos
         if (cnpj == "00000000000000" ||
                 cnpj == "11111111111111" ||
                 cnpj == "22222222222222" ||
@@ -419,7 +318,6 @@ var Formzin = {
                 cnpj == "99999999999999")
             return false;
 
-        // Valida DVs
         tamanho = cnpj.length - 2
         numeros = cnpj.substring(0, tamanho);
         digitos = cnpj.substring(tamanho);
@@ -450,14 +348,34 @@ var Formzin = {
         return true;
 
     },
-    validarData: function(valor) { // Crédito ao site http://www.codigocomcafe.com/2009/08/validando-datas-com-javascript-e-regex/
-        regex = /^((((0?[1-9]|1\d|2[0-8])\/(0?[1-9]|1[0-2]))|((29|30)\/(0?[13456789]|1[0-2]))|(31\/(0?[13578]|1[02])))\/((19|20)?\d\d))jQuery|((29\/0?2\/)((19|20)?(0[48]|[2468][048]|[13579][26])|(20)?00))jQuery/;
-
-        resultado = regex.exec(valor);
-        if (!resultado)
+    validarData: function(valor) {
+        var currVal = valor;
+        if (currVal == '')
             return false;
-        else
-            return true;
+
+        var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+        var dtArray = currVal.match(rxDatePattern);
+
+        if (dtArray == null)
+            return false;
+
+        dtMonth = dtArray[3];
+        dtDay = dtArray[1];
+        dtYear = dtArray[5];
+
+        if (dtMonth < 1 || dtMonth > 12)
+            return false;
+        else if (dtDay < 1 || dtDay > 31)
+            return false;
+        else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31)
+            return false;
+        else if (dtMonth == 2)
+        {
+            var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+            if (dtDay > 29 || (dtDay == 29 && !isleap))
+                return false;
+        }
+        return true;
     }
 };
 
